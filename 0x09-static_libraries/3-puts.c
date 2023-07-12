c9fa3144454fa0745635f14b78357ddbac20fb71
VengefulHummingbird0403 <hoveronoh@gmail.com>
2023-07-11 17:20:32 -0700

    Static Libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ * function is responsible for outputting a string to standard output
+ * Single Argument; pointer to an array(string)
+ * Return:void
+ */
+_puts(char *s)
+#include<stdio.h>
+/**
+*  * main - Entry point
+*   *
+*    * Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
